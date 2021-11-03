57b60f7b5c4cd4d2a8f07e72dbc393e948614340
SoNesky <gueronlj@gmail.com>
2021-11-03 18:31:32 -0400

    edit form works

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ const App = () => {
    const [ messages, setMessages ] = useState([])
    const [ createdAuthor, setcreatedAuthor ] = useState('')
    const [createdMessage, setcreatedMessage ] = useState('')
+   const [ targetId, settargetId ] = useState('')
+   const [ editOn, seteditOn ] = useState(false)
+   const [ targetAuthor, settargetAuthor ] = useState('')
+   const [ targetMessage, settargetMessage ] = useState('')
+
 
    const updateAuthor = (event) => {
       setcreatedAuthor(event.target.value)
+      settargetAuthor(event.target.value)
    }
 
    const updateMessage = (event) => {
       setcreatedMessage(event.target.value)
+      settargetMessage(event.target.value)
    }
 
    useEffect(() => {
@@ -55,7 +62,28 @@ const App = () => {
          })
    }
 
+   const handleEditForm = (event) => {
+      event.preventDefault()
+      axios.put(`http://localhost:3001/chatrooms/${targetId}`,
+         {
+            username:createdAuthor||targetAuthor,//if there was nothing typed in the input, use previous value
+            message:createdMessage||targetMessage,
+         }
+      ).then(() => {
+         axios
+            .get('http://localhost:3001/chatrooms')
+            .then((response) => {
+               // console.log(response);
+               setMessages(response.data)
+            })
+      })
+   }
+
    const handleEditButton = (message) => {
+      settargetId(message._id)
+      editOn===true?(seteditOn(false)):(seteditOn(true))
+      settargetAuthor(message.username)
+      settargetMessage(message.message)
    }
 
    return(
@@ -77,7 +105,18 @@ const App = () => {
                            <h5>{message.username}</h5>
                         </div>
                         <div className='cardBody'>
-                           <p>{message.message}</p>
+                        { message.editOn?
+                           (<div id="editForm">
+                              <h4>Edit</h4>
+                              <form onSubmit={handleEditForm}>
+                                 Username: <input type='text' onChange={updateAuthor} value={targetAuthor}/><br/>
+                                 <textarea onChange={updateMessage} value={targetMessage}/><br/>
+                                 <input type='submit' value='send'/>
+                              </form>
+                           </div>):
+                           (<p>{message.message}</p>)
+                        }
+
                         </div>
                         <div className='cardLower'>
                            <p>(timestamp here)</p>
@@ -96,15 +135,16 @@ const App = () => {
                })}
             </div>
             <div className='right'>
-               <h4>Send a message...</h4>
-               <form onSubmit={handleSendBtn}>
-                  Username: <input type='text' onChange={updateAuthor}/><br/>
-                  <input type='text-area' onChange={updateMessage} /><br/>
-                  <input type='submit' value='send'/>
-               </form>
+
             </div>
          </div>
          <footer>
+         <h4>Send a message...</h4>
+         <form onSubmit={handleSendBtn}>
+            Username: <input type='text' onChange={updateAuthor}/><br/>
+            <textarea onChange={updateMessage} /><br/>
+            <input type='submit' value='send'/>
+         </form>
          </footer>
       </main>
    )
