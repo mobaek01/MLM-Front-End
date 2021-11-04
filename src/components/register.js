511d495a7f6b25cb92ddeced68c0633ed9aa18eb
SoNesky <gueronlj@gmail.com>
2021-11-04 10:48:19 -0400

    login and register form

---
+++ src/components/register.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {useState} from 'react'
+import axios from 'axios'
+
+const Register = () => {
+   const [ newUser, setnewUser] = useState('')
+   const [ newPassword, setnewPassword ] = useState('')
+   const updatenewUser = (event) => {
+      setnewUser(event.target.value)
+   }
+
+   const updatenewPassword = (event) => {
+      setnewPassword(event.target.value)
+   }
+
+   const handleRegistration = (event) => {
+      setnewUser(event)
+      axios.post(`http://localhost:3001/users`,
+         {
+            username:newUser,
+            message:newPassword,
+         }
+      ).then(() => {
+         axios
+            .get('http://localhost:3001/')
+            .then((response) => {
+               // console.log(response);
+               console.log(response);
+            })
+      })
+   }
+
+   return (
+      <>
+         <h4>Make an account</h4>
+         <form onSubmit={handleRegistration}>
+            Username: <input type='text' onChange={updatenewUser}/><br/>
+            Password: <input type='text' onChange={updatenewPassword}/><br/>
+            <input type='submit'/>
+         </form>
+      </>
+   )
+}
+
+export default Register
