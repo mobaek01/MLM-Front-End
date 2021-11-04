5423c6e3eabc65ca8614c72853524398c7fe1713
Moses Baek <moses.baek09@gmail.com>
2021-11-04 14:09:37 -0400

    log in function working

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Login = () => {
+
+    const [currentUser, setCurrentUser] = useState({username:"", password:""})
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const inputUsername = (event) => {
+        setUsername(event.target.value)
+    }
+    const inputPassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const checkCred = (event) => {
+        event.preventDefault()
+        setCurrentUser({username:username, password: password})
+        axios
+            .get(`http://localhost:3001/sessions/${username}/${password}`)
+            .then(console.log('trying to log in'))
+    }
+
    return (
       <>
          <h4>Log in</h4>
-         <form>
-            Username: <input type='text' /><br/>
-            Password: <input type='text' /><br/>
+         <form onSubmit={checkCred}>
+            Username: <input type='text' onChange={inputUsername}/><br/>
+            Password: <input type='text' onChange={inputPassword}/><br/>
             <input type='submit'/>
          </form>
       </>
